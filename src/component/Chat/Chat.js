adbbc438d70d97f9836499dd7d8f89a1e14f46f6
hugomatton <hugomattonisis@gmail.com>
2023-05-18 15:09:20 +0300

    chat en cours

--- src/component/Chat/Chat.js
+++ src/component/Chat/Chat.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ListChat from './ListChat/ListChat'
+import axios from 'axios'
+import { Row, Col, Spinner } from 'react-bootstrap'
+import Conversation from './Conversation/Conversation'
 
 const Chat = () => {
-  return (
-    <div>
-        <ListChat/>
-    </div>
-  )
-}
+    const [isLoading, setIsLoading] = useState(false);
+    const [conversations, setConversations] = useState([]);
+    const [selectedConversation, setSelectedConversation] = useState();
 
-export default Chat
+    const getConversations = async function () {
+        setIsLoading(true); // Définir isLoading à true avant de récupérer les conversations
+        try {
+            const result = await axios.get('http://localhost:5000/message/allConversationStudent?student_id=Student1');
+            setConversations(result.data);
+        } catch (error) {
+            console.error(error);
+        }
+        setIsLoading(false); // Définir isLoading à false une fois que les conversations ont été récupérées
+    };
+
+    useEffect(() => {
+        getConversations();
+    }, []);
+
+    return (
+        <div>
+            <h2 className="my-4 text-center">Chat ✉️</h2>
+            <Row>
+                <Col xs={3}>
+                    {isLoading ? (
+                        <Spinner animation="border" variant="primary" />
+                    ) : (
+                        <ListChat conversations={conversations} setSelectedConversation={setSelectedConversation} />
+                    )}
+                </Col>
+                <Col xs={9}>
+                    {
+                        selectedConversation && <Conversation />
+                    }
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default Chat;
--- src/component/Chat/ListChat/ListChat.js
+++ src/component/Chat/ListChat/ListChat.js
@@ -2,43 +2,36 @@ import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import Container from "react-bootstrap/Container";
 
-const ListChat = () => {
-  return (
-    <div>
-      <Container>
-        <h2 className="my-4 text-center">Chat ✉️</h2>
-        <ListGroup as="ol" numbered>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Owner 1</div>I am interested with your
-              housing
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Owner 2</div>
-              Would it be possible to lower the rental price?
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Owner 3</div>
-              Ok thank's
-            </div>
-          </ListGroup.Item>
-        </ListGroup>
-      </Container>
-    </div>
-  );
+const ListChat = ({ conversations, setSelectedConversation }) => {
+
+
+
+    return (
+        <div>
+            <Container>
+                <h3 className="my-4 text-center">Conversations</h3>
+                <ListGroup as="ol" numbered>
+                    {
+                        conversations.map((conversation) => {
+                            return (
+                                <ListGroup.Item
+                                    key={conversation[1]}
+                                    as="li"
+                                    className="d-flex justify-content-between align-items-start"
+                                    onClick={()=>{setSelectedConversation(conversation)}}
+                                >
+                                    <div className="ms-2 me-auto">
+                                        <div className="fw-bold">{conversation[0]}</div>I am interested with your
+                                        housing
+                                    </div>
+                                </ListGroup.Item>
+                            )
+                        })
+                    }
+                </ListGroup>
+            </Container>
+        </div>
+    );
 };
 
 export default ListChat;
